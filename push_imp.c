54534dd48b0a6fbe973829f43a88f44b3669e6cd
trendswave <paulchinedumicheal@gmail.com>
2023-11-17 01:32:01 +0100

    function that implement the push upcode

---
+++ push_imp.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * push - adds a node with the value token on the stack
+ * @stack: ponter to the list stack
+ * @l_num: unused variable
+ * Return:Retuns  address of a new node
+ */
+stack_t *push(stack_t **stack,
+	      unsigned int l_num __attribute__((unused)))
+{
+	stack_t *n_node;
+
+	/* Create new node */
+	n_node = malloc(sizeof(stack_t));
+	if (n_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	/* Initialize the new node with the value of token */
+	n_node->n = token;
+	/* New node is placed at the beginning of the list */
+	n_node->prv = NULL;
+	n_node->nxt = *stack;
+
+	/* If new node is not alone, update the following one */
+	if (*stack != NULL)
+		(*stack)->prv = n_node;
+
+	/* head points to the new node */
+	*stack = n_node;
+
+	return (n_node);
+}
