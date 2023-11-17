2ce2e8d01dd98bbdb38208ae06a3f842051555e2
trendswave <paulchinedumicheal@gmail.com>
2023-11-17 03:21:33 +0100

    Implement the mul opcode.

---
+++ mul_imp.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * mul_op - multiplies the second top element of the stack with the top one
+ * @stack: The stack list
+ * @line_num: The line number of the file
+ * Return: The edited stack
+ */
+
+stack_t *mul_op(stack_t **stack, unsigned int l_num)
+{
+	int outcome = 0;
+	stack_t *head = *stack;
+
+	/* Check if stack is NULL */
+	if (!head || !head->nxt)
+	{
+		/* Print error, free, and exit */
+		fprintf(stderr, "L%d: can't mul, stack too short\n", l_num);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	outcome = (head->nxt->n) * (head->n);
+	head->nxt->n = outcome;
+	pop(stack, 0);
+
+	return (*stack);
+}
