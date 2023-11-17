5611fa4acfb44599b6522f6f8b0fff6e8fb345f7
trendswave <paulchinedumicheal@gmail.com>
2023-11-17 05:24:56 +0100

    edidted pop file

--- pop_imp.c
+++ pop_imp.c
@@ -3,33 +3,33 @@
 /**
  * pop - This function deletes the node at the top of the stack
  * @stack: Pointer to the stack
- * @l_num: Line number of the file
- * Return: Return Pointer to the stack
+ * @line_number: Line number of the file
+ * Return: Pointer to the stack
  */
 
 stack_t *pop(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	/* pointer to head of stack */
+	/* Head points to head of stack */
 	stack_t *head = *stack;
 
-	/* Check NULL stack */
+	/* Check if stack is NULL */
 	if (*stack == NULL)
 	{
-		/* Prints error, free, and exit */
+		/* Print error, free, and exit */
 		fprintf(stderr, "L%d: can't pop an empty stack\n",
-			l_num);
+			line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	/* Stack pointing to the next node */
-	*stack = (*stack)->nxt;
+	/* Stack now points to the next node */
+	*stack = (*stack)->next;
 
-	/* Free beginning head node */
+	/* Free head node */
 	free(head);
 
-	/*Set it's previous pointer to NULL if available*/
+	/* If stack exists already, set it's prev pointer to NULL */
 	if (*stack)
-		(*stack)->prv = NULL;
+		(*stack)->prev = NULL;
 
 	return (*stack);
 }
