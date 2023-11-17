6ac1159a390d5b53174495b5694d56197e28b91c
trendswave <paulchinedumicheal@gmail.com>
2023-11-17 01:41:05 +0100

    Function that Implement the swap opcode.

---
+++ swap_imp.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: ponter to the list stack
+ * @l_num: unused variable
+ * Return: Returns address of the first node
+ */
+stack_t *swap(stack_t **stack,
+	      unsigned int l_num __attribute__((unused)))
+{
+	stack_t *head;
+	int temp;
+
+	head = *stack;
+
+	if (head != NULL && head->nxt != NULL)
+	{
+		temp = head->n;
+		head->n = head->nxt->n;
+		head->nxt->n = temp;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n",
+			l_num);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	return (*stack);
+}
