286d8aadf860131315dad103653f781c9208f142
trendswave <paulchinedumicheal@gmail.com>
2023-11-17 01:04:30 +0100

    pall implememntation function

---
+++ pall_imp.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * pall - The function that print all
+ * @stack: pointer to the list stack
+ * @l_numb: unused variable(line number)
+ * Return: the address of the first node
+ */
+stack_t *pall(stack_t **stack,
+	      unsigned int l_numb __attribute__((unused)))
+{
+	stack_t *temp;
+	size_t node = 0;
+
+	temp = *stack;
+
+	if (*stack == NULL)
+	{
+		return (NULL);
+	}
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->nxt;
+		node++;
+	}
+
+	return (*stack);
+}
